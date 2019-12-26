4a98782a57531a2cae9a667275ff9a8ba7ed3041
Alvaro <alvgaona@gmail.com>
2019-12-26 19:20:18 -0300

    Added .clang-format and reformatted files

--- src/route_planner.h
+++ src/route_planner.h
@@ -2,36 +2,39 @@
 #define ROUTE_PLANNER_H
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+
 #include "route_model.h"
 
-
 class RoutePlanner {
-  public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
-    float GetDistance() const {return distance;}
-    void AStarSearch();
-    static bool CompareNodesByFValue(const RouteModel::Node* a, const RouteModel::Node* b);
+ public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x,
+               float end_y);
+  // Add public variables or methods declarations here.
+  float GetDistance() const { return distance; }
+  void AStarSearch();
+  static bool CompareNodesByFValue(const RouteModel::Node *a,
+                                   const RouteModel::Node *b);
 
-    // The following methods have been made public so we can test them individually.
-    void AddNeighbors(RouteModel::Node *current_node);
-    float CalculateHValue(RouteModel::Node const *node);
-    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
-    RouteModel::Node *NextNode();
+  // The following methods have been made public so we can test them
+  // individually.
+  void AddNeighbors(RouteModel::Node *current_node);
+  float CalculateHValue(RouteModel::Node const *node);
+  std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  RouteModel::Node *NextNode();
 
-  private:
-    // Add private variables or methods declarations here.
-    std::vector<RouteModel::Node*> open_list;
-    RouteModel::Node *start_node;
-    RouteModel::Node *end_node;
+ private:
+  // Add private variables or methods declarations here.
+  std::vector<RouteModel::Node *> open_list;
+  RouteModel::Node *start_node;
+  RouteModel::Node *end_node;
 
-    float distance = 0.0f;
-    RouteModel &m_Model;
+  float distance = 0.0f;
+  RouteModel &m_Model;
 
-    bool IsStartNode(const RouteModel::Node *node);
-    bool IsEndNode(const RouteModel::Node *node);
+  bool IsStartNode(const RouteModel::Node *node);
+  bool IsEndNode(const RouteModel::Node *node);
 };
 
 #endif
